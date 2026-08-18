b47e3ba49e0f5875abb1120dd0ffb68be458c5ce
abdulrehman7876 <abdulre7876@gmail.com>
2026-08-18 09:37:06 +0500

    Todo list added and amazon project reat router fix

---
+++ 05Amazon-Clone/src/Hooks/useProductId.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function useItemProduct(id) {
+  const [categoryData, setCategoryData] = useState([]);
+
+  useEffect(() => {
+    const fetchCategoryData = async () => {
+      try {
+        const results = await Promise.all(
+          id.map(async (elm) => {
+            const res = await fetch(`https://dummyjson.com/products/${elm}`);
+            return res.json();
+          }),
+        );
+        setCategoryData(results);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchCategoryData();
+  }, [id]);
+  return categoryData;
+}
+
+export default useItemProduct;
